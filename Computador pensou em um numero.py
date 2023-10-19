8b626c20b281a2e80fedf7411d59d2999f31204d
Yuri <140124162+Yuzodin@users.noreply.github.com>
2023-10-19 07:09:50 -0300

    Add files via upload
    Versão 2.0 do jogo onde você tenta adivinhar o numero que o computador pensou.

--- Computador pensou em um numero.py
+++ Computador pensou em um numero.py
@@ -1,13 +1,28 @@
+tenta = 0
 import random 
 from time import sleep
-pn= random. randint(0,5)
+pc= random. randint(0,10)
 print('='*41)
-print('Estou pensando em um numero entre 0 e 5..')
+print('Estou pensando em um numero entre 0 e 10..')
 print('='*41)
 print('Processando...')
 sleep(2)
-r= int(input ('Digite um numero entre 0 e 5: '))
-if r == pn:
-    print('Parabens você conseguiu me vencer. O numero era {}'.format (pn))
-else:
-    print('Ha, Ha, Ha ganhei pensei no numero {} não no {}'.format (pn,r))
+acertou = False
+while not acertou:
+    player = int(input ('Tente acertar o numero que o computador pensou!! Digite um numero entre 0 e 10: '))
+    tenta += 1
+    if player == pc:
+        acertou = True
+        print('Parabens você conseguiu vencer o computador. O numero era {}'.format (pc))
+        print('Foram necessarias: {} tentativas para acertar'.format(tenta))  
+    else:
+        if player < pc:
+            print('O numero que pensei é maior... Tente novamnete: ')
+        else:
+            print('O numero que pensei é menor... Tente  novamente: ')    
+        
+       
+
+
+
+    
